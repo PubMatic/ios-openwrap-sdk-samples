341e1862f6b982b5180345b010f002a8e106e57e
Pritesh Lad <pritesh.lad@pubmatic.com>
2019-02-20 21:04:38 +0530

    updated SampleApp-Bridging-Header.h in swift sample app

--- OpenBid/DFP/Swift/SampleApp/SampleApp/AppDelegate/SampleApp-Bridging-Header.h
+++ OpenBid/DFP/Swift/SampleApp/SampleApp/AppDelegate/SampleApp-Bridging-Header.h
@@ -30,9 +30,6 @@
  Use this file to import your target's public headers that you would like to expose to Swift.
  */
 
-#import <POBRenderer.h>
-#import <POBBid.h>
-
 //Banner Ad
 #import <POBBannerView.h>
 
